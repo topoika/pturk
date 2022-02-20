8f2206b5668409fec052c007b12207ec175f45cf
topoika <topoikatampul@gmail.com>
2022-02-21 00:03:26 +0300

    search page coming

--- components/search/Results.js
+++ components/search/Results.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import Recent from "./../../public/static/images/ls.jpg";
+
 function Results() {
   return (
     <div className="flex justify-start p-4 border rounded-sm border-opacity-40 w-full hover:shadow-md cursor-pointer">
       <div className="">
-        <p>Image here</p>
+        <Image src={Recent} alt="New listing" />
       </div>
       <div>
         <p>Info here</p>
