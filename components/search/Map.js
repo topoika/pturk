c2a119d9056210c94e4d0ad092bba1567f22ad48
topoika <topoikatampul@gmail.com>
2022-02-23 08:39:07 +0300

    leaflet added

--- components/search/Map.js
+++ components/search/Map.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Map() {
   return (
     <div>
-      <p>This is the map sectiopm</p>
+      <MapContainer
+        center={[40.8054, -74.0241]}
+        zoom={14}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
+          <Popup>Hey ! you found me</Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
