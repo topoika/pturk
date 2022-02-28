fa79a3bc810f653bb5a924f783aed41e33267fb5
topoika <topoikatampul@gmail.com>
2022-02-28 16:01:33 +0300

    big bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         "hero-pattern": "url('./../public/static/images/bg.jpg')",
+        "hero-pattern":
+          " url(`https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_large_assets/e456d8b923f5/assets/img/home/hero_photos/DLIUyjgS7v50YRV2xfNquA.jpg`)",
       },
     },
   },
